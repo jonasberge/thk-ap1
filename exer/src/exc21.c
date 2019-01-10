fcbf2ca0170bbcd2b7b907263e2b03287ed39c5d
snrk0 <jonas.vanen@gmail.com>
2019-01-10 20:26:22 +0100

    Implemented actual program

--- exer/src/exc21.c
+++ exer/src/exc21.c
@@ -9,17 +9,21 @@
 
 int main(void)
 {
-	char name1[BUFSZ_NAME], name2[BUFSZ_NAME];
-	
-	str_read2(name1, BUFSZ_NAME, name2, BUFSZ_NAME);
-	printf("name1: '%s'\nname2: '%s'\n", name1, name2);
+	puts("Programm zur Ermittlung von Freunden.");
+	printf("INFO: ein Name hat eine maximale Länge "
+		"von %zu Zeichen.\n", NAME_LEN);
 
-	int equal = str_iequal(name1, name2);
-	printf("are the names equal? %s\n", equal ? "yes" : "no");
+	char name1[BUFSZ_NAME], name2[BUFSZ_NAME];
+	str_read2(name1, BUFSZ_NAME, name2, BUFSZ_NAME);
+
+	if (str_iequal(name1, name2)) {
+		printf("%s hat keine Freunde.\n", name1);
+		return 0;
+	}
 
 	char names[BUFSZ_CONCAT];
 	str_concat(name1, name2, names, BUFSZ_CONCAT);
-	printf("concated names: '%s'\n", names);
+	printf("%s sind Freunde\n", names);
 
 	return 0;
 }
