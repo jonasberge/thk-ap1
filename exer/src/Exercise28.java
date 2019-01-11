dae1112bc781f860d137a72c8b4d7621c423ab3f
snrk0 <jonas.vanen@gmail.com>
2019-01-11 01:13:47 +0100

    Implemented exercise 28

---
+++ exer/src/Exercise28.java
@@ -0,0 +1,32 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Exercise28
+{
+	public static int getProductionYear()
+	{
+		System.out.print("Bitte geben Sie das Produktionsjahr "
+			+ "(in der Form yyyy) ein: ");
+		return new Scanner(System.in).nextInt();
+	}
+
+	public static int calculateAge(int productionYear)
+	{
+		Calendar now = Calendar.getInstance();
+		int today = now.get(Calendar.YEAR);
+		return today - productionYear;
+	}
+
+	public static void printAge(int age)
+	{
+		System.out.printf("Das Auto ist heute %d Jahre alt.\n", age);
+	}
+
+	public static void main(String[] args)
+	{
+		int year = getProductionYear();
+		int age = calculateAge(year);
+		printAge(age);
+	}
+}
+
