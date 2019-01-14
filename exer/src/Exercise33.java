42e6bc3b1dbf9ad7edda02c1e85348b8a243696a
snrk0 <jonas.vanen@gmail.com>
2019-01-15 00:11:27 +0100

    Christmas tree generator implementation

---
+++ exer/src/Exercise33.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercise33
+{
+	private static final char CHAR_NEEDLE = '*';
+	private static final char CHAR_TRUNK = '#';
+	private static final char CHAR_AIR = ' ';
+
+	private static void printChar(char c, int n)
+	{
+		for (int i = 0; i < n; ++i)
+			System.out.print(c);
+	}
+
+	public static boolean drawTree(int height)
+	{
+		if (height < 2) return false;
+
+		int crownHeight = height - 1;
+		for (int i = 0; i < crownHeight; ++i) {
+			int numAir = crownHeight - i - 1;
+			int numNeedle = 2 * i + 1;
+			printChar(CHAR_AIR, numAir);
+			printChar(CHAR_NEEDLE, numNeedle);
+			System.out.println();
+		}
+
+		printChar(CHAR_AIR, crownHeight - 1);
+		System.out.println(CHAR_TRUNK);
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int height;
+
+		do {
+			System.out.print("Geben Sie eine Höhe ein: ");
+			height = in.nextInt();
+		} while (!drawTree(height));
+	}
+}
