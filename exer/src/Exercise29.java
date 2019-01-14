a99043c34d9b0e1039784e2c21c0844fefe3d54b
snrk0 <jonas.vanen@gmail.com>
2019-01-14 21:40:16 +0100

    Added javadoc comments and make target

---
+++ exer/src/Exercise29.java
@@ -0,0 +1,51 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ * Ein Programm zur Berechnung des Alters eines Autos,
+ * mithilfe dessen Produktionsjahr.
+ * @author Tadeg Jonas van den Berg
+ * @author www.gm.fh-koeln.de/~inf1424
+ */
+public class Exercise29
+{
+	/**
+	 * Liest das Produktionsjahr eines Autos von der Tastatur ein.
+	 * @return das Produktionsjahr des Autos.
+	 */
+	public static int getProductionYear()
+	{
+		System.out.print("Bitte geben Sie das Produktionsjahr "
+			+ "(in der Form yyyy) ein: ");
+		return new Scanner(System.in).nextInt();
+	}
+
+	/**
+	 * Berechnung des Alters eines Autos anhand dessen Produktionsjahr.
+	 * @param productionYear das Jahr in welchem das Auto produziert wurde.
+	 * @return das Alter des Autos
+	 */
+	public static int calculateAge(int productionYear)
+	{
+		Calendar now = Calendar.getInstance();
+		int today = now.get(Calendar.YEAR);
+		return today - productionYear;
+	}
+
+	/**
+	 * Gibt das Alter des Autos aus.
+	 * @param age das Alter des Autos.
+	 */
+	public static void printAge(int age)
+	{
+		System.out.printf("Das Auto ist heute %d Jahre alt.\n", age);
+	}
+
+	public static void main(String[] args)
+	{
+		int year = getProductionYear();
+		int age = calculateAge(year);
+		printAge(age);
+	}
+}
+
--- exer/src/java.mak
+++ exer/src/java.mak
@@ -1,13 +1,19 @@
 BINDIR=../bin
+HTMLDIR=${HOME}/public_html
+DOCDIR=$(HTMLDIR)/doc
 
 PREF=Exercise
 JAVAC=javac
+JAVADOC=javadoc
 FLAGS=-d $(BINDIR)
 
 all: $(patsubst %.java,%,$(wildcard $(PREF)*.java))
 
 $(PREF)%:
 	$(JAVAC) $@.java $(FLAGS)
+
+$(PREF)29: 
+	$(JAVADOC) -d $(DOCDIR)/$(shell echo $@ | tr A-Z a-z) $@.java
 
 .PHONY: clean
 clean:
