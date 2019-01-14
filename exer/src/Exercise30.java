5a45a7d19b6a5b13cfef618ee899254493cfcfe4
snrk0 <jonas.vanen@gmail.com>
2019-01-14 23:01:30 +0100

    Added implementation of exercise 30

---
+++ exer/src/Exercise30.java
@@ -0,0 +1,22 @@
+import java.lang.Math.*;
+import java.util.Scanner;
+
+public class Exercise30
+{
+	private static final Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		System.out.println("Programm zur Berechnung der Potenz x^y");
+		
+		System.out.print("Bitte geben Sie x ein: ");
+		double x = in.nextDouble();
+
+		System.out.print("Bitte geben Sie y ein: ");
+		double y = in.nextDouble();
+
+		double power = Math.pow(x, y);
+		System.out.printf("Die Potenz von x^y ist: %f%n", power);
+	}
+}
+
