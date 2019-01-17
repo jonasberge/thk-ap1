2b33ae0e2a046dae600c0094d3e287b475f10ae1
snrk0 <jonas.vanen@gmail.com>
2019-01-17 13:28:44 +0100

    Fixed total width

--- exer/src/exc1.c
+++ exer/src/exc1.c
@@ -12,14 +12,14 @@ int main(void)
 	fputs("Bitte geben sie die zweite Seite in cm ein: ", stdout);
 	scanf("%f", &b);
 
-	printf("\nSeite 1 ist %5.2f cm lang.\n", a);
-	printf("Seite 2 ist %5.2f cm lang.\n", b);
+	printf("\nSeite 1 ist %8.2f cm lang.\n", a);
+	printf("Seite 2 ist %8.2f cm lang.\n", b);
 
 	float perimeter = 2*a + 2*b;
 	float area = a*b;
 
-	printf("\nDer Umfang des Recktecks beträgt %5.2f cm.\n", perimeter);
-	printf("Die Fläche des Rechtecks beträgt %5.2f cm².\n", area);
+	printf("\nDer Umfang des Recktecks beträgt %8.2f cm.\n", perimeter);
+	printf("Die Fläche des Rechtecks beträgt %8.2f cm².\n", area);
 
 	return 0;
 }
