9c92daea16d45f375b58c8f6dd9bc117b3bd6e41
snrk0 <jonas.vanen@gmail.com>
2019-01-18 14:20:05 +0100

    Added JavaDoc comments

--- exer/src/Rational.java
+++ exer/src/Rational.java
@@ -1,14 +1,27 @@
+/**
+ * Klasse zur Darstellung eines Bruchs bzw. einer rationalen Zahl.
+ * @author Tadeg Jonas van den Berg
+ * @author www.gm.fh-koeln.de/~inf1424
+ */
 public class Rational
 {
 	private int numerator;
 	private int denominator;
 
+	/**
+	 * Erstellt einen Bruch mit entsprechendem Zähler und Nenner.
+	 * @param numerator der Zähler.
+	 * @param denominator der Nenner.
+	 */
 	public Rational(int numerator, int denominator)
 	{
 		this.numerator = numerator;
 		this.denominator = denominator;
 	}
 
+	/**
+	 * Gibt den Bruch gekürzt auf der Konsole aus.
+	 */
 	public void print()
 	{
 		simplify();
@@ -16,6 +29,9 @@ public class Rational
 			numerator, denominator);
 	}
 
+	/**
+	 * Kürzt den Bruch.
+	 */
 	private void simplify()
 	{
 		int divisor = gcd(numerator, denominator);
@@ -23,6 +39,11 @@ public class Rational
 		denominator /= divisor;
 	}
 
+	/**
+	 * Berechnet den größten gemeinsamen Teiler (ggT) zweier Zahlen.
+	 * @param a die erste Zahl.
+	 * @param b die zweite Zahl.
+	 */
 	private int gcd(int a, int b)
 	{
 		while (b != 0) {
